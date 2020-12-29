c99b50778a14fb0e84781ee78cc9f3c1fd62dde6
Mark Horsell <mark.horsell@gmail.com>
2020-12-29 00:26:20 +0000

    forecast

---
+++ src/Weather.tsx
@@ -0,0 +1,58 @@
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
+
+export default function Weather() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
+  );
+}
+
+function Example(): any {
+  //code is for azure id - IS NOT the weather data secret key
+
+const fetchString=`https://markweather.azurewebsites.net/api/HttpTrigger2?code=bEePfjWLznUduliGYYUHeDGbzXLtSyCbPjgVyIe/Zh0iTlITyjywJA==`;
+//const fetchString=`http://localhost:7071/api/HttpTrigger2`;
+
+  const { isLoading, error, data } = useQuery("weatherData", () =>
+    fetch(
+      fetchString
+    ).then((res) => 
+  
+      res.json()
+    )
+    
+      
+  );
+  //https://markweather.azurewebsites.net/api/HttpTrigger2?code=bEePfjWLznUduliGYYUHeDGbzXLtSyCbPjgVyIe/Zh0iTlITyjywJA==
+
+  if (isLoading) return "Loading...";
+
+  if (error) {
+    const e: any = error;
+    return "An error has occurred x: " + e.message;
+  }
+  if (data) {
+    console.log(data);
+  }
+
+  return (
+    <div style={{ padding: "0px", border: "1px solid grey" }}>
+ 
+    <div>
+    {data.list.map((dp:any) =>{
+      return <div>
+        {dp.dt_txt}{" "}
+        {dp.weather[0].main}
+        </div>
+    })}
+    </div>
+  
+
+      <ReactQueryDevtools initialIsOpen />
+    </div>
+  );
+}
